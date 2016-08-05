b7a4e882876185462314ae8db6d7edb84db0eeca
Wolfgang Warneke <wolfgang.warneke@gmail.com>
2016-08-05 14:54:48 -0700

    add functionality to create object with user name and array of repository objects with name, creation date, and description

--- js/githubapi-interface.js
+++ js/githubapi-interface.js
@@ -1,15 +1,20 @@
 var GitHubUserRepositories = require('./../js/githubapi.js').GitHubUserRepositoriesModule;
+var GetRepos = require('./../js/githubapi.js').getRepos;
 var apiKey = require('./../.env').apiKey;
+
 
 $(document).ready(function() {
   $('#userReposQuery').submit(function(event) {
     event.preventDefault();
     var userNameQuery = $('#userName').val();
-    var newUserRepositories = new GitHubUserRepositories(userNameQuery);
-    console.log(newUserRepositories);
+    var userRepos;
+    var gettingRepos = new GetRepos(userNameQuery);
+    gettingRepos.done(function(repos) {
+      console.log("fun");
+      userRepos = new GitHubUserRepositories(userNameQuery, repos);
+    });
     var htmlOutput = "<table><tr><td>Repo Name</td><td>Repo Description</td></tr>";
     htmlOutput += "</table>";
-    console.log(htmlOutput);
     $('#repositoryInfoOutput').html(htmlOutput);
   });
 });
--- js/githubapi.js
+++ js/githubapi.js
@@ -1,16 +1,28 @@
 var apiKey = require('./../.env').apiKey;
 
-function GitHubUserRepositories(userName) {
-  var retrievedRepositories = this.getRepos(userName);
+function GitHubUserRepositories(userName, retrievedRepos) {
+  var self = this;
+  var retrievedRepositories = retrievedRepos;
+  self.userName = userName;
+  self.userRepositories = [];
+  retrievedRepositories.forEach(function(retrievedRepo) {
+    var repository = {};
+    repository['name'] = retrievedRepo.name;
+    repository['description'] = retrievedRepo.description;
+    repository['creationDate'] = retrievedRepo.created_at;
+    self.userRepositories.push(repository);
+    console.log(retrievedRepo);
+  });
 }
 
-GitHubUserRepositories.prototype.getRepos = function(userName){
-  $.get('https://api.github.com/users/' + userName + '/repos?access_token=' + apiKey).then(function(response){
-    console.log(response);
+function GetRepos(userName){
+   return $.get('https://api.github.com/users/' + userName + '/repos?access_token=' + apiKey).then(function(response){
     return response;
   }).fail(function(error){
     console.log(error.responseJSON.message);
+    console.log("failure");
   });
 };
 
+exports.getRepos = GetRepos;
 exports.GitHubUserRepositoriesModule = GitHubUserRepositories;
